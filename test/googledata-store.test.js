7a4f30c901d87a1adf15ea98da584404d58b3078
John Roche <john.roche@nearform.com>
2014-06-09 11:06:23 +0100

    removed unused tests

--- test/googledata-store.test.js
+++ test/googledata-store.test.js
@@ -1,5 +1,5 @@
 /* Copyright (c) 2014 John Roche */
-"use strict";
+'use strict';
 
 
 var seneca = require('seneca');
@@ -9,7 +9,9 @@ var si = seneca();
 
 var SCOPES = ['https://www.googleapis.com/auth/userinfo.email',
               'https://www.googleapis.com/auth/datastore'];
-
+/**
+* Enter your Google Datastore details
+*/
 var options = {
                 'DATASTORE_SERVICE_ACCOUNT': '',
                 'DATASTORE_PRIVATE_KEY_FILE': '',
@@ -19,8 +21,8 @@ var options = {
 
 si.use(require('..'), options);
 
-si.__testcount = 0
-var testcount = 0
+si.__testcount = 0;
+var testcount = 0;
 
 describe('googledata', function(){
   this.timeout(35000);
@@ -28,9 +30,9 @@ describe('googledata', function(){
   it('basic', function(done){
     testcount++;
     shared.basictest(si,done);
-  })
+  });
 
   it('close', function(done){
     shared.closetest(si,testcount,done);
-  })
-})
+  });
+});
